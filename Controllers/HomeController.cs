3d6d4164ef112f7faa5c6b13eb930620472b70f1
KarenCWLin1228 <karencw1228@gmail.com>
2022-05-04 18:18:34 +0800

    go

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,7 @@ namespace NewApp.Controllers
                 {
                     var article = new Article
                     {
+                        ID = 2,
                         Title = "test test",
                         ReleaseDate = DateTime.Parse("2020-09-03").ToString(),
                         Link = "https://ithelp.ithome.com.tw/articles/10233371",
